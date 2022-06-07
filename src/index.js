6afb087d9e8cf9878d58ca40c99bf6becebf7d0f
fmpereira2309 <fmpereira2309@gmail.com>
2022-06-07 18:40:13 -0300

    deploy2

--- src/index.js
+++ src/index.js
@@ -6,15 +6,11 @@ const routes = require('./routes');
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
+    res.header("Access-Control-Allow-Origin", "*");
+    app.use(cors());
     next();
 });
 
-app.use(cors());
 app.use(express.json());
 app.use(routes);
 
