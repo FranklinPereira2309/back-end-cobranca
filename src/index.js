cea612fe1cc0a1820d068329b02b6ef1726bb32d
fmpereira2309 <fmpereira2309@gmail.com>
2022-06-08 16:02:29 -0300

    deploy - alteração package-json2

--- src/index.js
+++ src/index.js
@@ -5,17 +5,8 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", {
-        "origin": "*",
-        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-        "preflightContinue": false,
-        "optionsSuccessStatus": 204
-    });
-    app.use(cors());
-    next();
-});
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
